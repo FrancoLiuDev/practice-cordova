d8cba3b76e8414e573f5d8c478248ccb697348d0
Your Name <you@example.com>
2018-03-02 18:01:21 +0800

    add component

---
+++ src/components/page/Vgadraw.vue
@@ -0,0 +1,111 @@
+<template>
+	<div>
+		<button v-on:click="onButtonClick">hide/show</button>
+		<svg height="50" width="50" style="background: red" @mouseover="mouseOver()" @mouseleave="mouseLeave()">
+			<path :d="'M20 '+vrat+' L40 0 L225 200 Z'" />
+
+		</svg>
+		<svg height="210" width="400">
+			<path v-show="svgShow" d="M150 20 L75 200 L225 200 Z" />
+
+		</svg>
+		<svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 816 1056" height="1056" width="816" xml:space="preserve" id="svg2" version="1.1">
+			<metadata id="metadata8">
+				<rdf:RDF>
+					<cc:Work rdf:about="">
+						<dc:format>image/svg+xml</dc:format>
+						<dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+					</cc:Work>
+				</rdf:RDF>
+			</metadata>
+			<defs id="defs6">
+				<clipPath id="clipPath18" clipPathUnits="userSpaceOnUse">
+					<path id="path16" d="M 0,792 H 612 V 0 H 0 Z" />
+				</clipPath>
+			</defs>
+			<g transform="matrix(1.3333333,0,0,-1.3333333,0,1056)" id="g10">
+				<g id="g12">
+					<g clip-path="url(#clipPath18)" id="g14">
+						<g transform="translate(275.166,294.4893)" id="g20">
+							<path id="path22" style="fill:#cccccc;fill-opacity:1;fill-rule:nonzero;stroke:none" d="M 0,0 C 17.292,82.004 6.182,183.57 14.816,259.536 1.843,257.165 -24.122,265.688 -23.731,252.057 -46.357,179.555 -54.429,81.594 -62.285,4.981 -53.976,3.653 -46.857,1.327 -38.549,0 c 9.673,71.71 5.576,155.012 20.736,222.108 -0.216,-55.678 -8.33,-113.457 -11.842,-167.205 -1.411,-21.27 -6.572,-45.336 5.924,-47.424 16.528,4.319 8.698,24.446 8.893,34.947 1.064,55.947 2.301,113.127 5.944,162.208 6.119,-51.787 -2.453,-121.805 0,-177.184 C -8.567,19.957 -16.207,1.454 0,0 m 53.366,269.53 c -22.019,-3.098 -58.942,6.341 -71.179,-4.997 22.02,3.1 58.947,-6.341 71.179,4.997 m 2.975,4.981 C 84.453,195.856 85.949,94.8 100.834,4.981 78.382,-8.936 62.455,2.341 35.601,0.064 54.235,47.012 27.834,145.701 44.473,204.634 45.079,157.956 43.672,94.847 44.473,47.407 44.731,32.228 38.007,9.36 56.341,7.479 70.484,69.703 44.799,156.152 53.366,222.108 61.675,150.901 63.26,74.036 68.204,0 78.814,0.206 85.344,3.876 94.89,4.981 81.636,90.325 81.068,186.353 53.366,259.536 41.608,258.618 24.948,261.83 17.788,257.054 v -4.997 c 9.046,-9.868 -4.709,-38.899 8.898,-44.925 C 16.487,163.505 23.711,120.764 23.711,72.374 23.711,47.611 14.533,8.869 35.448,0.064 30.894,-0.35 26.012,-1.14 20.741,-2.499 13.906,41.65 19.72,96.458 17.788,144.752 10.91,98.941 17.94,41.462 8.894,-2.499 c -31.215,8.854 -55.472,-6.942 -80.072,7.48 18.486,88.426 16.532,194.057 50.416,269.53 z" />
+						</g>
+						<g transform="translate(370.0557,299.4707)" id="g24">
+							<path id="path26" style="fill:#ffffff;fill-opacity:1;fill-rule:nonzero;stroke:none" d="m 0,0 c -9.546,-1.105 -16.075,-4.775 -26.686,-4.981 -4.944,74.036 -6.529,150.901 -14.837,222.108 -8.568,-65.957 17.118,-152.405 2.974,-214.629 -18.334,1.881 -11.609,24.748 -11.868,39.928 -0.801,47.439 0.606,110.548 0,157.226 -16.639,-58.933 9.763,-157.622 -8.872,-204.569 -0.043,0 -0.089,-0.019 -0.152,0 -20.915,8.805 -11.738,47.547 -11.738,72.31 0,48.39 -7.223,91.131 2.975,134.758 -13.607,6.025 0.148,35.057 -8.898,44.924 v 4.998 c 7.161,4.776 23.821,1.564 35.579,2.481 C -13.821,181.372 -13.254,85.344 0,0" />
+						</g>
+						<g transform="translate(257.3535,559.022)" id="g28">
+							<path id="path30" style="fill:#ffffff;fill-opacity:1;fill-rule:nonzero;stroke:none" d="M 0,0 C 12.236,11.338 49.159,1.899 71.179,4.997 58.946,-6.341 22.02,3.101 0,0" />
+						</g>
+						<g transform="translate(310.7666,294.5537)" id="g32">
+							<path id="path34" style="fill:#00ff00;fill-opacity:1;fill-rule:nonzero;stroke:none" d="M 0,0 C 0,-0.019 -0.021,-0.05 -0.021,-0.064 -0.063,-0.05 -0.11,-0.034 -0.152,0 -0.089,-0.019 -0.043,0 0,0" />
+						</g>
+						<g transform="translate(289.9824,554.0249)" id="g36">
+							<path id="path38" style="fill:#ffffff;fill-opacity:1;fill-rule:nonzero;stroke:none" d="m 0,0 c -8.635,-75.965 2.476,-177.532 -14.816,-259.536 -16.207,1.454 -8.568,19.957 -8.894,27.451 -2.453,55.378 6.118,125.397 0,177.183 -3.644,-49.081 -4.881,-106.261 -5.944,-162.208 -0.196,-10.501 7.634,-30.628 -8.894,-34.946 -12.495,2.088 -7.335,26.153 -5.924,47.424 3.513,53.748 11.626,111.527 11.843,167.205 -15.16,-67.096 -11.063,-150.399 -20.736,-222.109 -8.309,1.328 -15.427,3.654 -23.736,4.982 7.855,76.612 15.927,174.574 38.553,247.075 C -38.938,6.152 -12.974,-2.371 0,0" />
+						</g>
+					</g>
+				</g>
+			</g>
+			<path :d="'M20 '+vrat+' L40 0 L225 200 Z'" />
+		</svg>
+	</div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    components: {},
+    data() {
+        return {
+            svgShow: false,
+            vrat: '20'
+        }
+    },
+    methods: {
+        onButtonClick: function() {
+            console.log('plugins.popupmsg', modusecho)
+
+            modusecho.echo(
+                'native massage!',
+                function() {
+                    console.log('success')
+                },
+                function() {
+                    console.log('faild')
+                }
+            )
+        },
+        mouseOver: function() {
+            console.log('mouseOver')
+            this.svgShow = true
+            this.vrat = '0'
+        },
+        mouseLeave: function() {
+            console.log('mouseLeave')
+            this.svgShow = false
+            this.vrat = '20'
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bt-size {
+    width: 60px;
+    height: 35px;
+}
+h1,
+h2 {
+    font-weight: normal;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import VgaDraw from '@/components/page/Vgadraw'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld
+        },
+        {
+            path: '/vgadraw',
+            name: 'VgaDraw',
+            component: VgaDraw
+        }
+    ]
 })
